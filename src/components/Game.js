16d3c1afa4f0e53668338357199e5cf08faa3ffc
Michael Romashov <mromashov@icloud.com>
2020-03-29 01:48:04 -0400

    Value generation and click actions complete

--- src/components/Game.js
+++ src/components/Game.js
@@ -3,39 +3,83 @@ import Tile from './Tile.js'
 
 class Game extends React.Component {
     state = {
-        board: this.generateBoard(this.props.width, this.props.height, this.props.mines)
+        board: this.generateBoard(),
+        mined: false
     }
 
-    // Generates and populates game board with randomly placed mines
-    generateBoard(width, height, mines) {
+    // Generates empty game board as a matrix
+    generateBoard() {
         var board = []
-        var minesGenerated = 0
 
-        for (var y = 0; y < height; y++) {
+        for (var y = 0; y < this.props.height; y++) {
             var row = []
-            for (var x = 0; x < width; x++) { row.push(0) }
+            for (var x = 0; x < this.props.width; x++) { row.push({ mine: 0, value: 0 }) }
             board.push(row)
-        }
-
-        while (minesGenerated < mines) {
-            var randY = Math.floor(Math.random() * height)
-            var randX = Math.floor(Math.random() * width)
-            if (!board[randY][randX]) {
-                board[randY][randX] = 1
-                minesGenerated++
-            }
         }
 
         return board
     }
 
+    // Randomly places mines on game board
+    generateMines(originX, originY) {
+        if (!this.state.mined) {
+            var minesGenerated = 0
+            var minedBoard = this.state.board
+
+            while (minesGenerated < this.props.mines) {
+                var randY = Math.floor(Math.random() * this.props.height)
+                var randX = Math.floor(Math.random() * this.props.width)
+                if (!minedBoard[randY][randX].mine && randX !== Number(originX) && randY !== Number(originY)) {
+                    minedBoard[randY][randX].mine = 1
+                    minesGenerated++
+                }
+            }
+
+            this.setState({ board: minedBoard, mined: true })
+        }
+    }
+
+    // Generates values for tiles, which is how many bombs surround the tile
+    generateValues() {
+        var valueBoard = this.state.board
+        var perms = [{ dx: -1, dy: -1 }, { dx: -1, dy: 0 }, { dx: -1, dy: 1 }, { dx: 0, dy: -1 }, { dx: 0, dy: 1 }, { dx: 1, dy: -1 }, { dx: 1, dy: 0 }, { dx: 1, dy: 1 }]
+
+        for (var x = 0; x < this.props.width; x++) {
+            for (var y = 0; y < this.props.height; y++) {
+                var value = 0
+                if (valueBoard[y][x].mine === 0) {
+                    for (var i = 0; i < 8; i++) {
+                        var newX = x + perms[i].dx
+                        var newY = y + perms[i].dy
+                        if (newX >= 0 && newX < this.props.width && newY >= 0 && newY < this.props.height && this.state.board[newY][newX].mine === 1) { value++ }
+                    }
+                } else { value = null }
+
+                valueBoard[y][x].value = value
+            }
+        }
+
+        this.setState({ board: valueBoard })
+    }
+
     render() {
-        console.log(this.state.board)
+        var generateMines = this.generateMines
+        var generateValues = this.generateValues
+
         return (
             <div className="game">
-                {this.state.board.map(row =>
+                {this.state.board.map((row, indexY) =>
                     <div className="game-row d-flex">
-                        {row.map(tile => <Tile mine={tile} />)}
+                        {row.map((tile, indexX) =>
+                            <Tile
+                                generateMines={generateMines.bind(this)}
+                                generateValues={generateValues.bind(this)}
+                                mine={tile.mine}
+                                value={tile.value}
+                                x={indexX}
+                                y={indexY}
+                            />
+                        )}
                     </div>
                 )}
             </div>
--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,10 +1,42 @@
 import React from 'react'
 
 class Tile extends React.Component {
-    state = {}
+    state = {
+        revealed: false,
+        flag: false
+    }
+
+    // Sets tile state to revealed. If it is the first tile it generates the mines
+    reveal() {
+        this.props.generateMines(this.props.x, this.props.y)
+        this.props.generateValues()
+        this.setState({ revealed: true })
+    }
+
+    // Handles right click event to flag a tile
+    flag = (event) => {
+        event.preventDefault()
+        if (this.state.flag) {
+            this.setState({ flag: false })
+        } else {
+            this.setState({ flag: true })
+        }
+    }
 
     render() {
-        return <div className="tile">{this.props.mine}</div>
+        if (this.state.revealed) {
+            if (this.props.mine) {
+                return <div className="tile mine text-center">{this.props.value}</div>
+            } else {
+                return <div className="tile revealed text-center">{this.props.value}</div>
+            }
+        } else {
+            if (this.state.flag) {
+                return <div className="tile flag text-center" onClick={() => this.reveal()} onContextMenu={this.flag}></div>
+            } else {
+                return <div className="tile text-center" onClick={() => this.reveal()} onContextMenu={this.flag}></div>
+            }
+        }
     }
 }
 
